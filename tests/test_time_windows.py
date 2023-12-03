7a2bb55bf781e66eda1196b8c74e4ec546b20bf8
N. L. <nino@pleno.earth>
2023-12-03 14:04:27 +0100

    test(time_windows): create a unittest for time_window and compute_pot_windows
    The test should ensure that compute_pot_windows are executed from the correct set_time_window (overloaded function).

---
+++ tests/test_time_windows.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+from anomalytics.time_windows.pot_windows import compute_pot_windows
+from anomalytics.time_windows.time_window import set_time_window
+
+
+class TestTimeWindow(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_compute_pot_windows_executed_correctly_via_set_time_window(self):
+        t0, t1, t2 = set_time_window(
+            total_rows=1000, method="POT", analysis_type="historical", t0_pct=0.65, t1_pct=0.25, t2_pct=0.10
+        )
+        expected_t0, expected_t1, expected_t2 = compute_pot_windows(
+            total_rows=1000, analysis_type="historical", t0_pct=0.65, t1_pct=0.25, t2_pct=0.10
+        )
+        self.assertEqual(first=(t0 + t1 + t2), second=(expected_t0 + expected_t1 + expected_t2))
+        self.assertEqual(first=t0, second=expected_t0)
+        self.assertEqual(first=t1, second=expected_t1)
+        self.assertEqual(first=t2, second=expected_t2)
+
+    def test_pot_windows_for_historical_analysis_via_set_time_window(self):
+        t0, t1, t2 = set_time_window(
+            total_rows=1000, method="POT", analysis_type="historical", t0_pct=0.65, t1_pct=0.25, t2_pct=0.10
+        )
+        self.assertEqual(first=(t0 + t1 + t2), second=1000)
+        self.assertEqual(first=t0, second=650)
+        self.assertEqual(first=t1, second=250)
+        self.assertEqual(first=t2, second=100)
+
+    def test_pot_windows_for_realtime_analysis_via_set_time_window(self):
+        t0, t1, t2 = set_time_window(
+            total_rows=1000, method="POT", analysis_type="real-time", t0_pct=0.7, t1_pct=0.3, t2_pct=0.0
+        )
+        self.assertEqual(first=(t0 + t1 + t2), second=1000)
+        self.assertEqual(first=t0, second=699)
+        self.assertEqual(first=t1, second=300)
+        self.assertEqual(first=t2, second=1)
+
+    def tearDown(self) -> None:
+        return super().tearDown()
