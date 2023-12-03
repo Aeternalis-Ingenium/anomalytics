ee930f1b2726a85e464f01b5ed54bfaaf733205e
N. L. <nino@pleno.earth>
2023-12-03 14:33:10 +0100

    fix(pot_window): change name from pot_windows to pot_window

--- src/anomalytics/time_windows/__init__.py
+++ src/anomalytics/time_windows/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["compute_pot_windows", "set_time_window"]
 
-from anomalytics.time_windows.pot_windows import compute_pot_windows
+from anomalytics.time_windows.pot_window import compute_pot_windows
 from anomalytics.time_windows.time_window import set_time_window
