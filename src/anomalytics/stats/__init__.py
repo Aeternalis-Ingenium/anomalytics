4447dbe89b5e0dbb91fdf452ae7d4687d2dd06cb
N. L. <nino@pleno.earth>
2023-12-04 00:59:59 +0100

    feat(get_exceedance_peaks_over_threshold): create the function for exceedance extraction with POT method

--- src/anomalytics/stats/__init__.py
+++ src/anomalytics/stats/__init__.py
@@ -1,3 +1,6 @@
-__all__ = ["get_pot_threshold", "get_peaks_over_threshold"]
+__all__ = ["get_threshold_peaks_over_threshold", "get_exceedance_peaks_over_threshold"]
 
-from anomalytics.stats.peaks_over_threshold import get_peaks_over_threshold, get_pot_threshold
+from anomalytics.stats.peaks_over_threshold import (
+    get_exceedance_peaks_over_threshold,
+    get_threshold_peaks_over_threshold,
+)
--- src/anomalytics/stats/peaks_over_threshold.py
+++ src/anomalytics/stats/peaks_over_threshold.py
@@ -7,19 +7,22 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def get_pot_threshold(
-    ts: pd.Series, t0: int, anomaly_type: typing.Literal["high", "low"] = "high", q: float = 0.90
+def get_threshold_peaks_over_threshold(
+    ts: pd.Series,
+    t0: int,
+    anomaly_type: typing.Literal["high", "low"] = "high",
+    q: float = 0.90,
 ) -> pd.Series:
     """
-    Calculate the threshold value that will be used to extract the exceedances from the original dataset.
+    Calculate the POT threshold value that will be used to extract the exceedances from `ts` dataset.
 
     ## Parameters
     -------------
     ts : pandas.Series
-        The dataset with 1 feature and datetime index to calculate the threshold values.
+        The dataset with 1 feature and datetime index to calculate the quantiles.
 
     t0 : int
-        The time window used to define the dynamic expanding window to calculate the quantile score for each row.
+        Time window to find dynamic expanding period for calculating quantile score.
 
     q : float
         The quantile to use for thresholding, default 0.90.
@@ -47,5 +50,42 @@ def get_pot_threshold(
     return ts.expanding(min_periods=t0).quantile(q=q).bfill()
 
 
-def get_peaks_over_threshold(ts: pd.Series, q: float = 0.97) -> pd.Series:
-    return pd.Series()
+def get_exceedance_peaks_over_threshold(
+    ts: pd.Series,
+    t0: int,
+    anomaly_type: typing.Literal["high", "low"] = "high",
+    q: float = 0.90,
+) -> pd.Series:
+    """
+    Extract values from the `ts` dataset that exceed the POT threshold values.
+
+    ## Parameters
+    -------------
+    ts : pandas.Series
+        The dataset with 1 feature and datetime index to calculate the quantiles.
+
+    t0 : int
+        Time window to find dynamic expanding period for calculating quantile score.
+
+    q : float
+        The quantile to use for thresholding, default 0.90.
+
+    ## Returns
+    ----------
+    exceedances : pandas.Series
+        A Pandas Series with values exceeding the POT thresholds.
+    """
+    if anomaly_type not in ["high", "low"]:
+        raise ValueError(f"Invalid value! The `anomaly_type` argument must be 'high' or 'low'")
+    if not isinstance(ts, pd.Series):
+        raise TypeError("Invalid value! The `ts` argument must be a Pandas Series")
+    if t0 is None:
+        raise ValueError("Invalid value! The `t0` argument must be an integer")
+
+    pot_thresholds = get_threshold_peaks_over_threshold(ts=ts, t0=t0, anomaly_type=anomaly_type, q=q)
+
+    if anomaly_type == "high":
+        pot_exceedances = np.maximum(ts - pot_thresholds, 0.0)
+    else:
+        pot_exceedances = np.where(ts > pot_thresholds, 0.0, np.abs(ts - pot_thresholds))
+    return pd.Series(index=ts.index, data=pot_exceedances, name="exceedances")
