f8c05cdf99e6ae35b41be1e4a4200fa6524a0c9a
N. L <118660354+Aeternalis-Ingenium@users.noreply.github.com>
2023-12-03 14:30:47 +0100

    feat(pot): create t0 t1 t2 computation logic (#4)
    * ci(python-version): change "3.12.0" into "3.12"
    
    * feat(stats): create the main directory for all statistical methods
    
    * feat(time-windows): create time windows main directory
    
    This directory is to store the implementation of calculating the time windows needed for each statistical method e.g. POT with its t0, t1, t2 concept.
    
    * feat(pot_windows): implement logic for computing t0, t1, and t2 for POT method
    
    * feat(gitignore): add coverage files into the ignore list
    
    * feat(pot_windows): add error handler for if t0 - t1 < 0.0
    
    * feat(time_window): create main method to call all functions for time window setup
    
    The main idea is that each statistical method will have a different time window concept. Hence the repeated set_time_window as an overloaded function.
    
    * test(time_windows): create a unittest for time_window and compute_pot_windows
    
    The test should ensure that compute_pot_windows are executed from the correct set_time_window (overloaded function).
    
    * feat(plots): create plots/ initial directory
    
    * feat(evals): create evals/ initial directory for statistical evaluation
    
    * feat(import-strategy): import functions in time_windows into __init__.py
    
    set_time_window is now accessible from anomalytics/__init__.py and compute_pot_windows now accessible from anomalytics/time_windows/__init__.py
    
    * test(import): ensure time_windows functions are accessible via __init__.py

---
+++ src/anomalytics/time_windows/pot_windows.py
@@ -0,0 +1,32 @@
+import typing
+
+
+def compute_pot_windows(
+    total_rows: int,
+    analysis_type: typing.Literal["historical", "real-time"],
+    t0_pct: float = 0.65,
+    t1_pct: float = 0.25,
+    t2_pct: float = 0.10,
+) -> typing.Tuple[int, int, int]:
+    if t0_pct - t1_pct < 0.0:
+        raise ValueError("T0 time window needs to be bigger than T1 and T2, as a rule of thumb: t0 >= t1 > t2")
+    if analysis_type == "real-time":
+        if t0_pct + t1_pct != 1.0:
+            raise ValueError(
+                "In real-time analysis, the t2 time window will be the last row of the Time Series, hence `t0_pct` + `t1_pct` must equal to 1.0 (100%)"
+            )
+        t2 = 1
+        total_rows = total_rows - t2
+        t0 = int(t0_pct * total_rows)
+        t1 = int(t1_pct * total_rows)
+        uncounted_days = t0 + t1 - total_rows
+    else:
+        t0 = int(t0_pct * total_rows)
+        t1 = int(t1_pct * total_rows)
+        t2 = int(t2_pct * total_rows)
+        uncounted_days = t0 + t1 + t2 - total_rows
+    if uncounted_days < 0:
+        t1 += abs(uncounted_days)
+    elif uncounted_days > 0:
+        t1 -= uncounted_days
+    return (t0, t1, t2)
