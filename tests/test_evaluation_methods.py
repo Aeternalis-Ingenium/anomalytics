2c6bb68c70832de8c99087d25828f6f1c0bbe2d0
N. L. <nino@pleno.earth>
2023-12-04 05:55:31 +0100

    test(test_evaluation_methods): create mock test for qq-plot evaluation method

--- tests/test_evaluation_methods.py
+++ tests/test_evaluation_methods.py
@@ -1,9 +1,10 @@
 import unittest
+from unittest.mock import patch
 
 import numpy as np
 import pandas as pd
 
-from anomalytics.evals import ks_1sample
+from anomalytics.evals import calculate_theoretical_q, ks_1sample, visualize_qq_plot
 
 
 class TestEvaluationMethod(unittest.TestCase):
@@ -29,6 +30,36 @@ class TestEvaluationMethod(unittest.TestCase):
     def test_ks_1sample_with_unimplemented_method(self):
         with self.assertRaises(NotImplementedError):
             ks_1sample(ts=self.sample_ts, stats_method="AE", fit_params={})
+
+    def test_calculate_theoretical_q_pot(self):
+        sorted_nonzero_ts, theoretical_q, params = calculate_theoretical_q(
+            ts=self.sample_ts, stats_method="POT", fit_params=self.fit_params
+        )
+
+        self.assertIsInstance((sorted_nonzero_ts, theoretical_q, params), tuple)
+        self.assertEqual(len(sorted_nonzero_ts), len(theoretical_q))
+        self.assertEqual(params, self.fit_params[0])
+
+    def test_calculate_theoretical_q_with_invalid_series(self):
+        with self.assertRaises(TypeError):
+            calculate_theoretical_q(ts="not a series", stats_method="POT", fit_params=self.fit_params)
+
+    def test_calculate_theoretical_q_with_unimplemented_method(self):
+        with self.assertRaises(NotImplementedError):
+            calculate_theoretical_q(ts=self.sample_ts, stats_method="AE", fit_params=[{}])
+
+    @patch("matplotlib.pyplot.show")
+    def test_visualize_qq_plot_with_valid_input(self, mock_show):
+        visualize_qq_plot(ts=self.sample_ts, stats_method="POT", fit_params=self.fit_params)
+        mock_show.assert_called_once()
+
+    def test_visualize_qq_plot_with_invalid_series(self):
+        with self.assertRaises(TypeError):
+            visualize_qq_plot(ts="not a series", stats_method="POT", fit_params=self.fit_params)
+
+    def test_visualize_qq_plot_with_unimplemented_method(self):
+        with self.assertRaises(NotImplementedError):
+            visualize_qq_plot(ts=self.sample_ts, stats_method="AE", fit_params=self.fit_params)
 
     def tearDown(self) -> None:
         return super().tearDown()
