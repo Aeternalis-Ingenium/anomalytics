5498276dae6a29abeb4d097127717dcaa4bcb79a
N. L <118660354+Aeternalis-Ingenium@users.noreply.github.com>
2023-12-04 02:32:32 +0100

    feat(pot): create function for calculating anomaly score (#8)
    * feat(peaks_over_threshold): create fit function that utilises scipy.stats.genpareto.fit()
    
    The function is to calculate the anomaly score for each value in the exceedances pandas.Series. The anomaly score is calcualted by taking the result of the fit (c, loc, scale) and calculate the p-value using survival function before inverting it (1/p-value).
    
    * feat(import-strategy): add fit_exceedance into stats/__init__.py
    
    * feat(import-strategy): add fit_exceedance into __init__.py
    
    * test(test_peaks_over_threshold): ensure the fitting and anomaly score calculation are correct in fit function
    
    * fix(test_peaks_over_threshold): add ignore for mypy and complete the type for variables

--- src/anomalytics/stats/peaks_over_threshold.py
+++ src/anomalytics/stats/peaks_over_threshold.py
@@ -1,8 +1,10 @@
+import datetime
 import logging
 import typing
 
 import numpy as np
 import pandas as pd
+import scipy.stats as stats
 
 logger = logging.getLogger(__name__)
 
@@ -91,3 +93,78 @@ def get_exceedance_peaks_over_threshold(
         pot_exceedances = np.where(ts > pot_thresholds, 0.0, np.abs(ts - pot_thresholds))
     logger.debug(f"successfully extracting exceedances from dynamic threshold for {anomaly_type} anomaly type")
     return pd.Series(index=ts.index, data=pot_exceedances, name="exceedances")
+
+
+def fit_exceedance(ts: pd.Series, t0: int, gpd_params: typing.Dict) -> pd.Series:
+    """
+    Fit exceedances into generalized pareto distribution to calculate the anomaly score.
+
+    Anomaly Score = 1 / (1 - CDF(exceedance, c, loc, scale))
+
+    ## Parameters
+    -------------
+    ts : pandas.Series
+        The Pandas Series that contains the exceedances.
+
+    t0 : int
+        Time window to get the first day of t1 time window for dynamic window fitting.
+
+    gpd_params : dictionary
+        A dictionary used as the storage of the GPD parameters (fitting result).
+
+    ## Returns
+    ----------
+    anomaly_scores : pandas.Series
+        A Pandas Series with anomaly scores (inverted p-value) as its values.
+    """
+    logger.debug(
+        f"calculating anomaly score using t0={t0}, scipy.stats.genpareto.fit(), and scipy.stats.genpareto.sf()"
+    )
+    if not isinstance(ts, pd.Series):
+        raise TypeError("Invalid value! The `ts` argument must be a Pandas Series")
+    if t0 is None:
+        raise ValueError("Invalid value! The `t0` argument must be an integer")
+
+    anomaly_scores = []
+    t1_t2_exceedances = ts.iloc[t0:]
+
+    for row in range(0, t1_t2_exceedances.shape[0]):
+        fit_exceedances = ts.iloc[t0 + row :]
+        future_exeedance = t1_t2_exceedances.iloc[row]
+        nonzero_fit_exceedances = fit_exceedances[fit_exceedances.values > 0.0]
+        if future_exeedance > 0:
+            if len(nonzero_fit_exceedances.values) > 0:
+                (c, loc, scale) = stats.genpareto.fit(data=nonzero_fit_exceedances.values, floc=0)
+                p_value = stats.genpareto.sf(x=future_exeedance, c=c, loc=loc, scale=scale)
+                inverted_p_value = 1 / p_value if p_value > 0.0 else float("inf")
+                gpd_params[row] = dict(
+                    index=t1_t2_exceedances.index[row],
+                    c=c,
+                    loc=loc,
+                    scale=scale,
+                    p_value=p_value,
+                    anomaly_score=inverted_p_value,
+                )
+                anomaly_scores.append(inverted_p_value)
+            else:
+                gpd_params[row] = dict(
+                    index=t1_t2_exceedances.index[row],
+                    c=0.0,
+                    loc=0.0,
+                    scale=0.0,
+                    p_value=0.0,
+                    anomaly_score=0.0,
+                )
+                anomaly_scores.append(0.0)
+        else:
+            gpd_params[row] = dict(
+                index=t1_t2_exceedances.index[row],
+                c=0.0,
+                loc=0.0,
+                scale=0.0,
+                p_value=0.0,
+                anomaly_score=0.0,
+            )
+            anomaly_scores.append(0.0)
+    logger.debug(f"successfully calculating anomaly score")
+    return pd.Series(index=ts.index[t0:], data=anomaly_scores, name="anomaly scores")
--- tests/test_peaks_over_threshold.py
+++ tests/test_peaks_over_threshold.py
@@ -3,7 +3,7 @@ import unittest
 import numpy as np
 import pandas as pd
 
-from anomalytics import get_exceedance_peaks_over_threshold
+from anomalytics import fit_exceedance, get_exceedance_peaks_over_threshold
 from anomalytics.stats import get_threshold_peaks_over_threshold
 
 
@@ -60,6 +60,31 @@ class TestPeaksOverThreshold(unittest.TestCase):
     def test_invalid_t0_value_in_exceedance_extraction_function(self):
         with self.assertRaises(ValueError):
             get_exceedance_peaks_over_threshold(ts=self.sample_2_ts, t0=None, anomaly_type="high", q=0.90)  # type: ignore
+
+    def test_fit_exceedance_with_valid_input(self):
+        ts = pd.Series(np.random.rand(100) * 2, index=pd.date_range("2020-01-01", periods=100))
+        t0 = 10
+        gpd_params: dict = {}
+        exceedances = get_exceedance_peaks_over_threshold(ts=ts, t0=t0, anomaly_type="high", q=0.9)
+        anomaly_scores = fit_exceedance(ts=exceedances, t0=t0, gpd_params=gpd_params)
+
+        self.assertIsInstance(anomaly_scores, pd.Series)
+        self.assertEqual(len(anomaly_scores), len(ts.values) - t0)
+        self.assertTrue(all(isinstance(gpd_params[i], dict) for i in gpd_params))
+
+    def test_fit_exceedance_with_invalid_ts(self):
+        t0 = 10
+        gpd_params: dict = {}
+
+        with self.assertRaises(TypeError):
+            fit_exceedance(ts="not a series", t0=t0, gpd_params=gpd_params)
+
+    def test_fit_exceedance_with_invalid_t0(self):
+        ts = pd.Series(np.random.rand(100) * 2, index=pd.date_range("2020-01-01", periods=100))
+        gpd_params: dict = {}
+
+        with self.assertRaises(ValueError):
+            fit_exceedance(ts=ts, t0=None, gpd_params=gpd_params)  # type: ignore
 
     def tearDown(self) -> None:
         return super().tearDown()
