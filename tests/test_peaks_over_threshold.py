8867e3d89e65e011787eb098818491602a45c487
N. L. <nino@pleno.earth>
2023-12-04 01:02:10 +0100

    test(test_peaks_over_threshold): ensure that exceedance extraction provides the correct result
    The function get_exceedance_peaks_over_threshold executes the function get_threshold_peaks_over_threshold to get pot thresholds before subtracting the values with the respective threshold. Now, the low anomaly_type is a bit tricky, but it is solved! The trick is to consider exceedances as always a positive value because it is the difference between data value and threshold value.

--- tests/test_peaks_over_threshold.py
+++ tests/test_peaks_over_threshold.py
@@ -1,35 +1,64 @@
 import unittest
 
+import numpy as np
 import pandas as pd
 
-from anomalytics.stats import get_pot_threshold
+from anomalytics.stats import get_exceedance_peaks_over_threshold, get_threshold_peaks_over_threshold
 
 
 class TestPeaksOverThreshold(unittest.TestCase):
     def setUp(self):
-        self.ts = pd.Series(data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=pd.date_range(start="2023-01-01", periods=10))
+        self.sample_1_ts = pd.Series(
+            data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], index=pd.date_range(start="2023-01-01", periods=10)
+        )
+        self.sample_2_ts = pd.Series(np.random.rand(100), index=pd.date_range(start="2023-01-01", periods=100))
 
-    def test_high_anomaly_type(self):
-        pot_threshold = get_pot_threshold(self.ts, t0=3, anomaly_type="high", q=0.90)
+    def test_calculate_threshold_for_high_anomaly_type(self):
+        pot_threshold = get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="high", q=0.90)
         self.assertIsInstance(pot_threshold, pd.Series)
-        self.assertEqual(len(pot_threshold), len(self.ts))
+        self.assertEqual(len(pot_threshold), len(self.sample_1_ts))
 
-    def test_low_anomaly_type(self):
-        pot_threshold = get_pot_threshold(self.ts, t0=3, anomaly_type="low", q=0.10)
+    def test__calculate_threshold_for_low_anomaly_type(self):
+        pot_threshold = get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="low", q=0.10)
         self.assertIsInstance(pot_threshold, pd.Series)
-        self.assertEqual(len(pot_threshold), len(self.ts))
+        self.assertEqual(len(pot_threshold), len(self.sample_1_ts))
 
-    def test_invalid_anomaly_type(self):
+    def test_invalid_anomaly_type_in_threshold_calculation_function(self):
         with self.assertRaises(ValueError):
-            get_pot_threshold(self.ts, t0=3, anomaly_type="invalid", q=0.90)
+            get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="invalid", q=0.90)  # type: ignore
 
-    def test_invalid_ts_type(self):
+    def test_invalid_ts_type_in_threshold_calculation_function(self):
         with self.assertRaises(TypeError):
-            get_pot_threshold([1, 2, 3, 4], t0=3, anomaly_type="high", q=0.90)
+            get_threshold_peaks_over_threshold(ts=[1, 2, 3, 4], t0=3, anomaly_type="high", q=0.90)
 
-    def test_invalid_t0_value(self):
+    def test_invalid_t0_value_in_threshold_calculation_function(self):
         with self.assertRaises(ValueError):
-            get_pot_threshold(self.ts, t0=None, anomaly_type="high", q=0.90)
+            get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=None, anomaly_type="high", q=0.90)  # type: ignore
+
+    def test_extract_exceedance_for_high_anomaly_type(self):
+        pot_exceedance = get_exceedance_peaks_over_threshold(ts=self.sample_2_ts, t0=5, anomaly_type="high", q=0.90)
+        self.assertIsInstance(pot_exceedance, pd.Series)
+        self.assertEqual(len(pot_exceedance), len(self.sample_2_ts))
+        self.assertTrue((pot_exceedance >= 0).all())
+
+    def test_extract_exceedance_for_low_anomaly_type(self):
+        pot_exceedance = get_exceedance_peaks_over_threshold(ts=self.sample_2_ts, t0=5, anomaly_type="low", q=0.10)
+        self.assertIsInstance(pot_exceedance, pd.Series)
+        self.assertEqual(len(pot_exceedance), len(self.sample_2_ts))
+        # Check if all exceedances are non-negative
+        self.assertTrue((pot_exceedance >= 0).all())
+
+    def test_invalid_anomaly_type_in_exceedance_extraction_function(self):
+        with self.assertRaises(ValueError):
+            get_exceedance_peaks_over_threshold(ts=self.sample_2_ts, t0=5, anomaly_type="invalid", q=0.90)  # type: ignore
+
+    def test_invalid_ts_type_in_exceedance_extraction_function(self):
+        with self.assertRaises(TypeError):
+            get_exceedance_peaks_over_threshold(ts="Not a series", t0=5, anomaly_type="high", q=0.90)  # type: ignore
+
+    def test_invalid_t0_value_in_exceedance_extraction_function(self):
+        with self.assertRaises(ValueError):
+            get_exceedance_peaks_over_threshold(ts=self.sample_2_ts, anomaly_type="high", q=0.90)  # type: ignore
 
     def tearDown(self) -> None:
         return super().tearDown()
