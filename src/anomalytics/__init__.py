4a3ad5fc043338843d27ed3474373dcef180801d
N. L. <nino@pleno.earth>
2023-12-04 01:24:10 +0100

    test(test_time_series): ensure the read_ts wrapper function initializes the correct pd.Series
    read_ts is an overload and wrapper function for pd.read_csv and pd.read_excel.

--- src/anomalytics/__init__.py
+++ src/anomalytics/__init__.py
@@ -2,4 +2,6 @@ __version__ = "0.1.0"
 
 __all__ = ["set_time_window"]
 
+from anomalytics.stats import get_exceedance_peaks_over_threshold
+from anomalytics.time_series import read_ts
 from anomalytics.time_windows import set_time_window
---
+++ src/anomalytics/time_series/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["create_ts_from_csv", "create_ts_from_xlsx", "read_ts"]
+
+from anomalytics.time_series.upload import create_ts_from_csv, create_ts_from_xlsx, read_ts
