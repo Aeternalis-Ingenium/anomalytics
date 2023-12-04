045bc143b473aa3e6e70a1d06ae66110e5ab3a6c
N. L. <nino@pleno.earth>
2023-12-04 02:04:43 +0100

    feat(import-strategy): add fit_exceedance into stats/__init__.py

--- src/anomalytics/__init__.py
+++ src/anomalytics/__init__.py
@@ -1,7 +1,12 @@
 __version__ = "0.1.0"
 
-__all__ = ["set_time_window"]
+__all__ = [
+    "fit_exceedance",
+    "get_exceedance_peaks_over_threshold",
+    "read_ts",
+    "set_time_window"
+]
 
-from anomalytics.stats import get_exceedance_peaks_over_threshold
+from anomalytics.stats import fit_exceedance, get_exceedance_peaks_over_threshold
 from anomalytics.time_series import read_ts
 from anomalytics.time_windows import set_time_window
