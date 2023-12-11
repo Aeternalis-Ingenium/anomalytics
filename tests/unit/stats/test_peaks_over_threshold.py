4e08cdae73a398d7666ff090335ed2ba86f035f3
N. L. <nino@pleno.earth>
2023-12-11 19:31:37 +0100

    test(pot_detector): adjust unit test to take in the new parameter threshold_dataset for standalone functions

--- tests/unit/stats/test_peaks_over_threshold.py
+++ tests/unit/stats/test_peaks_over_threshold.py
@@ -20,38 +20,40 @@ class TestPeaksOverThreshold(unittest.TestCase):
         )
 
     def test_calculate_threshold_for_high_anomaly_type(self):
-        pot_threshold = get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="high", q=0.90)  # type: ignore
+        pot_threshold = get_threshold_peaks_over_threshold(dataset=self.sample_1_ts, t0=3, anomaly_type="high", q=0.90)  # type: ignore
         self.assertIsInstance(pot_threshold, pd.Series)
         self.assertEqual(len(pot_threshold), len(self.sample_1_ts))  # type: ignore
 
     def test__calculate_threshold_for_low_anomaly_type(self):
-        pot_threshold = get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="low", q=0.10)  # type: ignore
+        pot_threshold = get_threshold_peaks_over_threshold(dataset=self.sample_1_ts, t0=3, anomaly_type="low", q=0.10)  # type: ignore
         self.assertIsInstance(pot_threshold, pd.Series)
         self.assertEqual(len(pot_threshold), len(self.sample_1_ts))  # type: ignore
 
     def test_invalid_anomaly_type_in_threshold_calculation_function(self):
         with self.assertRaises(ValueError):
-            get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=3, anomaly_type="invalid", q=0.90)  # type: ignore
+            get_threshold_peaks_over_threshold(dataset=self.sample_1_ts, t0=3, anomaly_type="invalid", q=0.90)  # type: ignore
 
     def test_invalid_ts_type_in_threshold_calculation_function(self):
         with self.assertRaises(TypeError):
-            get_threshold_peaks_over_threshold(ts=[1, 2, 3, 4], t0=3, anomaly_type="high", q=0.90)
+            get_threshold_peaks_over_threshold(dataset=[1, 2, 3, 4], t0=3, anomaly_type="high", q=0.90)
 
     def test_invalid_t0_value_in_threshold_calculation_function(self):
         with self.assertRaises(ValueError):
-            get_threshold_peaks_over_threshold(ts=self.sample_1_ts, t0=None, anomaly_type="high", q=0.90)  # type: ignore
+            get_threshold_peaks_over_threshold(dataset=self.sample_1_ts, t0=None, anomaly_type="high", q=0.90)  # type: ignore
 
     def test_extract_exceedance_for_high_anomaly_type(self):
+        pot_threshold = get_threshold_peaks_over_threshold(dataset=self.random_sample_2_ts, t0=5, anomaly_type="high", q=0.90)  # type: ignore
         pot_exceedance = get_exceedance_peaks_over_threshold(
-            ts=self.random_sample_2_ts, t0=5, anomaly_type="high", q=0.90
+            dataset=self.random_sample_2_ts, threshold_dataset=pot_threshold, t0=5, anomaly_type="high", q=0.90
         )
         self.assertIsInstance(pot_exceedance, pd.Series)
         self.assertEqual(len(pot_exceedance), len(self.random_sample_2_ts))
         self.assertTrue((pot_exceedance >= 0).all())
 
     def test_extract_exceedance_for_low_anomaly_type(self):
+        pot_threshold = get_threshold_peaks_over_threshold(dataset=self.random_sample_2_ts, t0=5, anomaly_type="low", q=0.10)  # type: ignore
         pot_exceedance = get_exceedance_peaks_over_threshold(
-            ts=self.random_sample_2_ts, t0=5, anomaly_type="low", q=0.10
+            dataset=self.random_sample_2_ts, t0=5, threshold_dataset=pot_threshold, anomaly_type="low", q=0.10
         )
         self.assertIsInstance(pot_exceedance, pd.Series)
         self.assertEqual(len(pot_exceedance), len(self.random_sample_2_ts))
@@ -59,21 +61,32 @@ class TestPeaksOverThreshold(unittest.TestCase):
 
     def test_invalid_anomaly_type_in_exceedance_extraction_function(self):
         with self.assertRaises(ValueError):
-            get_exceedance_peaks_over_threshold(ts=self.random_sample_2_ts, t0=5, anomaly_type="invalid", q=0.90)  # type: ignore
+            get_exceedance_peaks_over_threshold(
+                dataset=self.random_sample_2_ts,
+                threshold_dataset=self.random_sample_2_ts,
+                t0=5,
+                anomaly_type="invalid",
+                q=0.90,
+            )  # type: ignore
 
     def test_invalid_ts_type_in_exceedance_extraction_function(self):
         with self.assertRaises(TypeError):
-            get_exceedance_peaks_over_threshold(ts="Not a series", t0=5, anomaly_type="high", q=0.90)  # type: ignore
+            get_exceedance_peaks_over_threshold(
+                dataset="Not aseries", threshold_dataset="Not aseries", t0=5, anomaly_type="high", q=0.90
+            )  # type: ignore
 
     def test_invalid_t0_value_in_exceedance_extraction_function(self):
         with self.assertRaises(ValueError):
-            get_exceedance_peaks_over_threshold(ts=self.random_sample_2_ts, t0=None, anomaly_type="high", q=0.90)  # type: ignore
+            get_exceedance_peaks_over_threshold(
+                dataset=self.random_sample_2_ts, threshold_dataset=[1, 2, 3, 4], t0=None, anomaly_type="high", q=0.90
+            )  # type: ignore
 
     def test_fit_exceedance_with_valid_input(self):
         t0 = 10
         gpd_params: dict = {}
+        pot_threshold = get_threshold_peaks_over_threshold(dataset=self.random_sample_3_ts, t0=t0, anomaly_type="high", q=0.90)  # type: ignore
         exceedances = get_exceedance_peaks_over_threshold(
-            ts=self.random_sample_3_ts, t0=t0, anomaly_type="high", q=0.9
+            dataset=self.random_sample_3_ts, threshold_dataset=pot_threshold, t0=t0, anomaly_type="high", q=0.90
         )
         anomaly_scores = get_anomaly_score(ts=exceedances, t0=t0, gpd_params=gpd_params)
 
